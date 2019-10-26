aeee66a7ed9398ac88442ebd2e6692c77be3e308
Eastern Lai <easternlai@hotmail.com>
2019-10-26 14:12:33 -0700

    created javascript code

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var apiKey = "OisMhG5CRGOmgESGOsDUeKF55eCQbZQl";
-var searchItem = $("#searchTerm").val();
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchItem + "&api-key=";
+console.log("test");
+
+$("#search-button").on("click", function(event){
+   
+    event.preventDefault();
 
 
-$.ajax({
-    url: queryURL + apiKey,
-    method: "GET"
-}).then(function(response) {
+
+    var apiKey = "OisMhG5CRGOmgESGOsDUeKF55eCQbZQl";
+    var searchItem = $("#searchTerm").val();
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+ searchItem + "&api-key=";
 
 
+    $.ajax({
+        url: queryURL + apiKey,
+        method: "GET"
+    }).then(function(response) {
+
+
+
+    });
 
 });
-
